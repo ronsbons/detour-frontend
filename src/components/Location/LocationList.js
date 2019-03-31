18c1cdd29f76d0a6dc6fd8fb23521d1f7791b7c8
ronsbons <ronnilouie@gmail.com>
2019-03-31 16:40:42 -0700

    sets up location directory

---
+++ src/components/Location/LocationList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LocationList extends Component {
+  render() {
+    return (
+      <div>
+        <p>hi</p>
+      </div>
+    );
+  }
+}
+
+export default LocationList;
