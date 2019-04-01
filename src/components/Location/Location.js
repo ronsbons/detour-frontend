f7c0bc37230136bff8fbeeb926b911432938019e
ronsbons <ronnilouie@gmail.com>
2019-03-31 21:59:35 -0700

    fixes className attribute

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -12,7 +12,7 @@ class Location extends Component {
           <div className="tile is-child is-4">
             {/* [] NEED TO CHANGE LINK PATHS */}
             <Link to="/">
-              <figure class="image is-128x128">
+              <figure className="image is-128x128">
                 <img src="./images/sho-hatakeyama-117306-unsplash.jpg" alt="man in Kenya by Sho Hatakeyama" />
               </figure>
               <h6 className="subtitle is-6">Africa</h6>
@@ -21,7 +21,7 @@ class Location extends Component {
 
           <div className="tile is-child is-4">
             <Link to="/">
-              <figure class="image is-128x128">
+              <figure className="image is-128x128">
                 <img src="./images/jack-ward-522993-unsplash.jpg" alt="Cinque Terre by Jack Ward" />
               </figure>
               <h6 className="subtitle is-6">Europe</h6>
@@ -29,10 +29,10 @@ class Location extends Component {
           </div>
         </div>
 
-        <div class="tile is-parent is-vertical">
+        <div className="tile is-parent is-vertical">
           <div className="tile is-child is-4">
             <Link to="/">
-              <figure class="image is-128x128">
+              <figure className="image is-128x128">
                 <img src="./images/lisheng-chang-396821-unsplash.jpg" alt="floating market by Lisheng Chang" />
               </figure>
               <h6 className="subtitle is-6">Asia</h6>
@@ -41,7 +41,7 @@ class Location extends Component {
 
           <div className="tile is-child is-4">
             <Link to="/">
-              <figure class="image is-128x128">
+              <figure className="image is-128x128">
                 <img src="./images/guillaume-jaillet-421771-unsplash.jpg" alt="maple leaf by Guillaume Jaillet" />
               </figure>
               <h6 className="subtitle is-6">North America</h6>
@@ -52,7 +52,7 @@ class Location extends Component {
         <div className="tile is-parent is-vertical">
           <div className="tile is-child is-4">
             <Link to="/">
-              <figure class="image is-128x128">
+              <figure className="image is-128x128">
                 <img src="./images/christopher-burns-429015-unsplash.jpg" alt="kangaroo at golden hour by Christopher Burns" />
               </figure>
               <h6 className="subtitle is-6">Australia</h6>
@@ -61,7 +61,7 @@ class Location extends Component {
 
           <div className="tile is-child is-4">
             <Link to="/">
-              <figure class="image is-128x128">
+              <figure className="image is-128x128">
                 <img src="./images/eduardo-flores-759719-unsplash.jpg" alt="Machu Picchu by Eduardo Flores" />
               </figure>
               <h6 className="subtitle is-6">South America</h6>
