c1c61f7a400de05f48422e31ee7b4d8ffd8c6ddf
ronsbons <ronnilouie@gmail.com>
2019-03-31 20:29:47 -0700

    adds alt text

--- src/components/About.js
+++ src/components/About.js
@@ -6,29 +6,29 @@ class About extends Component {
       <div className="about">
         <div className="columns">
           <div className="column">
-            <img src="./images/contiki-logo-vector.svg" />
+            <img src="./images/contiki-logo-vector.svg" alt="Contiki logo" />
             <h4 className="subtitle is-4">Contiki</h4>
           </div>
 
           <div className="column">
-            <img src="./images/g-adventures-seeklogo.com.svg" />
+            <img src="./images/g-adventures-seeklogo.com.svg" alt="G Adventures logo" />
             <h4 className="subtitle is-4">G Adventures</h4>
           </div>
 
           <div className="column">
-            <img src="./images/g-adventures-seeklogo.com.svg" />
+            <img src="./images/flashpack.png" alt="Flash Pack logo" />
             <h4 className="subtitle is-4">Flash Pack</h4>
           </div>
 
           <div className="column">
-            <img src="./images/g-adventures-seeklogo.com.svg" />
-            <h4 className="subtitle is-4">Top Deck</h4>
+            <img src="./images/topdeck-travel-logo.png" alt="Topdeck logo" />
+            <h4 className="subtitle is-4">Topdeck</h4>
           </div>
         </div>
 
         <h3 className="subtitle is-3">DeTour</h3>
         <h5 className="subtitle is-5">De-mystify all of your tour options</h5>
-        <p className="has-text-centered">Compare tours from the best companies offering group travel for 18-35 year olds or for 18-35 year olds at heart.</p>
+        <p className="has-text-centered">Compare tours from the best companies offering group travel for millennials and Gen Z-ers.</p>
       </div>
     );
   }
