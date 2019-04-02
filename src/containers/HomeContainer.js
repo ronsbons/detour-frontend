427867c2302efe1a188021136e1531e14d5dfff9
ronsbons <ronnilouie@gmail.com>
2019-04-01 17:23:40 -0700

    tries to fix image path

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -9,28 +9,34 @@ class HomeContainer extends Component {
   state = {
     regions: [
       {
+        _id: 1,
         locationName: 'Africa',
-        locationPhoto: '../images/sho-hatakeyama-117306-unsplash.jpg'
+        locationPhoto: '/src/images/sho-hatakeyama-117306-unsplash.jpg'
       },
       {
+        _id: 2,
         locationName: 'Asia',
-        locationPhoto: '../images/lisheng-chang-396821-unsplash.jpg'
+        locationPhoto: '/src/images/lisheng-chang-396821-unsplash.jpg'
       },
       {
+        _id: 3,
         locationName: 'Australia',
-        locationPhoto: '../images/christopher-burns-429015-unsplash.jpg'
+        locationPhoto: '/src/images/christopher-burns-429015-unsplash.jpg'
       },
       {
+        _id: 4,
         locationName: 'Europe',
-        locationPhoto: '../images/jack-ward-522993-unsplash.jpg'
+        locationPhoto: '/src/images/jack-ward-522993-unsplash.jpg'
       },
       {
+        _id: 5,
         locationName: 'North America',
-        locationPhoto: '../images/guillaume-jaillet-421771-unsplash.jpg'
+        locationPhoto: '/src/images/guillaume-jaillet-421771-unsplash.jpg'
       },
       {
+        _id: 6,
         locationName: 'South America',
-        locationPhoto: '../images/agustin-diaz-185846-unsplash.jpg'
+        locationPhoto: '/src/images/agustin-diaz-185846-unsplash.jpg'
       }
     ]
   };
