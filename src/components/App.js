4bd2aa6e6c97735bacd5604937f576dce0d734d6
ronsbons <ronnilouie@gmail.com>
2019-04-01 20:45:25 -0700

    moves regions to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,41 @@ import Footer from './Footer.js';
 import '../styles/App.css';
 
 class App extends Component {
+  state = {
+    regions: [
+      {
+        _id: 1,
+        regionName: 'Africa',
+        regionPhoto: '/src/images/sho-hatakeyama-117306-unsplash.jpg'
+      },
+      {
+        _id: 2,
+        regionName: 'Asia',
+        regionPhoto: '/src/images/lisheng-chang-396821-unsplash.jpg'
+      },
+      {
+        _id: 3,
+        regionName: 'Australia',
+        regionPhoto: '/src/images/christopher-burns-429015-unsplash.jpg'
+      },
+      {
+        _id: 4,
+        regionName: 'Europe',
+        regionPhoto: '/src/images/jack-ward-522993-unsplash.jpg'
+      },
+      {
+        _id: 5,
+        regionName: 'North America',
+        regionPhoto: '/src/images/guillaume-jaillet-421771-unsplash.jpg'
+      },
+      {
+        _id: 6,
+        regionName: 'South America',
+        regionPhoto: '/src/images/agustin-diaz-185846-unsplash.jpg'
+      }
+    ]
+  };
+  
   componentDidMount() {
     // successful test to connect to backend
     axios.get('http://localhost:3001/').then(response => {
@@ -20,11 +55,11 @@ class App extends Component {
   render() {
     return (
       <div className="app-container">
-        <NavBar />
+        <NavBar regions={this.state.regions} />
         <Switch>
           <Route exact path="/"
                 render={() => {
-                  return <HomeContainer />;
+                  return <HomeContainer regions={this.state.regions} />;
                 }}
           />
 
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -5,49 +5,13 @@ import About from '../components/About.js';
 import RegionSquare from '../components/Location/RegionSquare';
 
 class HomeContainer extends Component {
-  // [] FIGURE OUT IMAGE PATHS
-  state = {
-    regions: [
-      {
-        _id: 1,
-        regionName: 'Africa',
-        regionPhoto: '/src/images/sho-hatakeyama-117306-unsplash.jpg'
-      },
-      {
-        _id: 2,
-        regionName: 'Asia',
-        regionPhoto: '/src/images/lisheng-chang-396821-unsplash.jpg'
-      },
-      {
-        _id: 3,
-        regionName: 'Australia',
-        regionPhoto: '/src/images/christopher-burns-429015-unsplash.jpg'
-      },
-      {
-        _id: 4,
-        regionName: 'Europe',
-        regionPhoto: '/src/images/jack-ward-522993-unsplash.jpg'
-      },
-      {
-        _id: 5,
-        regionName: 'North America',
-        regionPhoto: '/src/images/guillaume-jaillet-421771-unsplash.jpg'
-      },
-      {
-        _id: 6,
-        regionName: 'South America',
-        regionPhoto: '/src/images/agustin-diaz-185846-unsplash.jpg'
-      }
-    ]
-  };
-
   render() {
     return (
       <div>
         <Carousel />
         <div className="home-content">
           <About />
-          <RegionSquare regions={this.state.regions} />
+          <RegionSquare regions={this.props.regions} />
         </div>
       </div>
     );
