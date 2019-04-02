193038f71773190df3b6867750849b698a223a94
ronsbons <ronnilouie@gmail.com>
2019-04-01 20:26:52 -0700

    creates new component for countries to be displayed on region page

---
+++ src/components/Location/CountrySquare.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+import { currentCountry } from '../../actions/locationActions.js';
+
+import './Location.css';
+
+class CountrySquare extends Component {
+  render() {
+    return (
+      <div className="columns location-columns is-multiline">
+
+        {/* loop through the locations passed down from RegionContainer */}
+        {this.props.countries.map(country => (
+          <div className="column is-one-third" key={country.countryName}>
+            {/* .currentCountry is the action imported in */}
+            {/* sends country object clicked on to change state */}
+            <Link to="/country"
+                  onClick={() => this.props.currentCountry(country)}>
+              <figure className="image is-128x128">
+                <img src={country.countryPhoto} alt={country.countryName} />
+              </figure>
+              <h6 className="subtitle is-6">{country.countryName}</h6>
+            </Link>
+
+          </div>
+        ))}
+
+      </div>
+    )
+  }
+}
+
+// this component needs access to the store in order to change it onClick
+// [] CORRECT?
+// const mapStateToProps = (store) => {
+//   return {
+//     location: store.location
+//   }
+// }
+
+export default connect(
+  null,
+  { currentCountry }
+)(CountrySquare);
+// export default Location;
--- src/containers/RegionContainer.js
+++ src/containers/RegionContainer.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 
-import Location from '../components/Location/Location.js';
+import CountrySquare from '../components/Location/CountrySquare.js';
 import CountryModel from '../models/CountryModel.js';
 
 class RegionContainer extends Component {
+  // set countries state in this component, because the data only needs to be accessed in this component
   state = {
     countries: [],
   };
 
   componentDidMount() {
+    // axios call to backend to get countries with region_id that matches the currentRegion's (from store) id
     CountryModel.getCountriesByRegion(this.props.currentRegion.id)
       .then(response => {
         console.log(response.data);
+        // adds found countries to this.state
         this.setState({
           countries: response.data
         });
@@ -26,25 +29,22 @@ class RegionContainer extends Component {
 
   render() {
     return (
-      <div>
+      <div className="region-container">
         <p>hi</p>
         <h3 className="subtitle is-3">{ this.props.currentRegion.name }</h3>
-        <Location locations={this.state.countries} />
+        {/* pass found countries to CountrySquare to be mapped */}
+        <CountrySquare countries={this.state.countries} />
       </div>
     );
   }
 }
 
-// <Location locations={this.props.countries} />
-
-// [] NEEDS TO ACCESS CLICKED ON REGION INFO FROM STORE IN ORDER TO PASS DOWN COUNTRY INFO TO LOCATION COMPONENT
+// needs to access the current region (clicked on region) that's in store, so need mapStateToProps function
 const mapStateToProps = (store) => {
   return {
     currentRegion: store.location
   }
 }
-
-// use this.props.currentRegion.currentLocation_id (1 for Africa) to search db.Country
 
 export default connect(
   mapStateToProps
