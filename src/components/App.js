5c6975c119c3706e93ee2afb11e5ae88e7cd56bf
ronsbons <ronnilouie@gmail.com>
2019-04-02 11:19:42 -0700

    moves images to correct folders to be accessed by css vs components

--- src/components/App.js
+++ src/components/App.js
@@ -16,32 +16,32 @@ class App extends Component {
       {
         _id: 1,
         regionName: 'Africa',
-        regionPhoto: '/src/images/sho-hatakeyama-117306-unsplash.jpg'
+        regionPhoto: './images/sho-hatakeyama-117306-unsplash.jpg'
       },
       {
         _id: 2,
         regionName: 'Asia',
-        regionPhoto: '/src/images/lisheng-chang-396821-unsplash.jpg'
+        regionPhoto: './images/lisheng-chang-396821-unsplash.jpg'
       },
       {
         _id: 3,
         regionName: 'Australia',
-        regionPhoto: '/src/images/christopher-burns-429015-unsplash.jpg'
+        regionPhoto: './images/christopher-burns-429015-unsplash.jpg'
       },
       {
         _id: 4,
         regionName: 'Europe',
-        regionPhoto: '/src/images/jack-ward-522993-unsplash.jpg'
+        regionPhoto: './images/jack-ward-522993-unsplash.jpg'
       },
       {
         _id: 5,
         regionName: 'North America',
-        regionPhoto: '/src/images/guillaume-jaillet-421771-unsplash.jpg'
+        regionPhoto: './images/guillaume-jaillet-421771-unsplash.jpg'
       },
       {
         _id: 6,
         regionName: 'South America',
-        regionPhoto: '/src/images/agustin-diaz-185846-unsplash.jpg'
+        regionPhoto: './images/eduardo-flores-759719-unsplash.jpg'
       }
     ]
   };
