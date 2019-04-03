e29c55961184d3343dd00042dca4e119fdad8520
ronsbons <ronnilouie@gmail.com>
2019-04-03 16:04:14 -0700

    starts user reducer for signup/login forms

--- src/reducers/indexReducers.js
+++ src/reducers/indexReducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import location from './locationReducer.js';
+import user from './userReducer.js';
 
 const rootReducer = combineReducers({
-  location
+  location,
+  user
 });
 
 export default rootReducer
