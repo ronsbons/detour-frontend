02c11971bf221ff9da4258446f3ff56638ffdb59
ronsbons <ronnilouie@gmail.com>
2019-04-03 20:23:56 -0700

    defines handleSignUp function

--- src/components/UserAuth.js
+++ src/components/UserAuth.js
@@ -3,20 +3,33 @@ import { connect } from 'react-redux';
 
 import UserModel from '../models/UserModel.js';
 
-// need an action that sets isLoggedIn to true
-// need an action that sets user to response.data.user
-// import { action } from '.../actions'
+import { isUserLoggedIn } from '../actions/userActions.js';
 
 class UserAuth extends Component {
-  handleInput = event => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
-
   handleSignup = event => {
+    console.log(event);
     event.preventDefault();
-    UserModel.signUp()
+
+    let username = event.target[0].value;
+    let email = event.target[1].value;
+    let password = event.target[2].value;
+    console.log(username, email, password);
+    // store input in local state like project 2?
+    UserModel.signUp(username, email, password)
+      .then(response => {
+        console.log(response.data);
+        // store jwt in local storage token
+        // [] can store jwt in store if i figure out redux-persist
+        localStorage.token = response.signedJwt;
+
+        // [] create an action that changes isLoggedIn to true
+        // [] create an action that stores logged in user info (user._id, user.username)
+        // { isUserLoggedIn(response.user)}
+        this.props.isUserLoggedIn(response.data.user);
+        debugger;
+
+        // [] do i need the verify method?
+      })
   }
 
   render() {
@@ -25,7 +38,7 @@ class UserAuth extends Component {
         {/* Log in form */}
         <div className="column">
           <h4 className="subtitle is-4 has-text-centered">Log In</h4>
-          <form onSubmit={handleLogin}>
+          <form>
             <input type="text" name="username" placeholder="username" />
             <input type="password" name="password" placeholder="password" />
             <button type="submit">Log In</button>
@@ -34,8 +47,8 @@ class UserAuth extends Component {
 
         {/* Sign up form */}
         <div className="column">
-          <h4 className="subtitle is-4 has-text-centered">Log In</h4>
-          <form onSubmit={handleSignup}>
+          <h4 className="subtitle is-4 has-text-centered">Sign Up</h4>
+          <form onSubmit={this.handleSignup}>
             <input type="text" name="username" placeholder="username" />
             <input type="email" name="email" placeholder="email" />
             <input type="password" name="password" placeholder="password" />
@@ -44,17 +57,12 @@ class UserAuth extends Component {
         </div>
       </div>
     );
-  }
-}
-
-const mapStateToProps = (store) => {
-  return {
-
   };
 };
 
+
 export default connect(
-  mapStateToProps,
-  // { action }
+  null,
+  { isUserLoggedIn }
 // export UserAuth component with these connected to it
 )(UserAuth);
