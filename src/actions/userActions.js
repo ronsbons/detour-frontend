feb2e2c9c26a5662d4e4cf40ff7aea3f97880139
ronsbons <ronnilouie@gmail.com>
2019-04-03 21:25:37 -0700

    defines action and reducer case for logout

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,3 +1,4 @@
+// pass user info and isLoggedIn as true to reducer
 export function isUserLoggedIn(user) {
   return {
     type: 'IS_USER_LOGGED_IN',
@@ -5,6 +6,18 @@ export function isUserLoggedIn(user) {
       isLoggedIn: true,
       _id: user._id,
       username: user.username,
+    },
+  };
+};
+
+// to log out, change info to blank and isLoggedIn to false
+export function userLogOut() {
+  return {
+    type: 'USER_LOG_OUT',
+    user: {
+      isLoggedIn: false,
+      _id: '',
+      username: '',
     }
   }
 }
--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -2,10 +2,8 @@ const jwt = require('jsonwebtoken');
 
 // get jwt token from localstorage
 let userToken = localStorage.getItem('token');
-console.log(`userToken: ${userToken}`);
 // verify token against secret key to access user payload
 let verified = jwt.verify(userToken, 'essendon');
-console.log(`verified: ${verified}`);
 
 // if there is a token in localStorage, set defaultState to loggedin true
 const defaultState = userToken ? {
@@ -21,7 +19,9 @@ const defaultState = userToken ? {
 
 function userLoggedIn(state = defaultState, action) {
   switch(action.type) {
+    // if action type is is_user_logged_in,
     case 'IS_USER_LOGGED_IN':
+      // define new state with user info
       const loggedInState = {
         isLoggedIn: action.user.isLoggedIn,
         _id: action.user._id,
@@ -30,9 +30,18 @@ function userLoggedIn(state = defaultState, action) {
 
       console.log(`logged in user: ${loggedInState.username}`);
       return loggedInState;
-    
-      default:
-        return state;
+    // if action type is logout,
+    case 'USER_LOG_OUT':
+      // define new state with cleared user info
+      const loggedOutState = {
+        isLoggedIn: false,
+        _id: '',
+        username: '',
+      };
+
+      return loggedOutState;    
+    default:
+      return state;
   };
 };
 
