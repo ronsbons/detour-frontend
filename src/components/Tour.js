89b7b0ccf16665eefc9aa463760f986ad0b635e7
ronsbons <ronnilouie@gmail.com>
2019-04-05 11:41:44 -0700

    adds save tour button if user is logged in

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+
+// import any model needed for axios calls
+import UserModel from '../models/UserModel.js';
+
+
 class Tour extends Component {
+  saveTour = (event) => {
+    event.preventDefault();
+    console.log(event);
+    UserModel.addSavedTour(this.props.user._id, this.props.tour._id)
+      // response will be updated user
+      .then( (response) => {
+        console.log(`saveTour response: ${response.data}`);
+        // [] NEED USER-FACING MESSAGE LIKE "TOUR SAVED!"
+        // [] COULD IT OPEN A MODAL?
+      }).catch( (error) => {
+        console.log(`saveTour error: ${error}`);
+        // [] NEED USER-FACING ERROR MESSAGE
+      });
+  };
+
   render() {
+    // show Save to Wish List button if user is logged in
+    let saveTourButton = [];
+
+    if (this.props.user.isLoggedIn) {
+      saveTourButton.push(
+        <button onClick={this.saveTour} key="save-button">Save to Wish List</button>
+      );
+    };
+
     return (
       <div className="column is-one-fifth">
         <div className="tour-cell">
@@ -25,9 +55,22 @@ class Tour extends Component {
         <div className="tour-cell">
           <h6 className="subtitle is-6">{this.props.tour.countries_visited}</h6>
         </div>
+        
+        { saveTourButton }
       </div>
     );
   }
 }
 
-export default Tour;
+
+// puts the user object in store into this.props.user
+const mapStateToProps = (store) => {
+  return {
+    user: store.user,
+  };
+};
+
+// connects the store and added props to component to export
+export default connect(
+  mapStateToProps
+)(Tour);
