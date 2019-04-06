01a1921e0e3d936b2d33520513cf20a5c4a69e2f
ronsbons <ronnilouie@gmail.com>
2019-04-06 15:36:14 -0700

    get countries from db to dynamically create footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,65 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+
+import CountryModel from '../models/CountryModel.js';
+
+import { currentCountry } from '../actions/locationActions.js';
+
 class Footer extends Component {
+  state = {
+    countriesAfrica: [],
+    countriesAsia: [],
+    countriesAus: [],
+    countriesEU: [],
+    countriesNAmer: [],
+    countriesSAmer: [],
+  };
+
+  componentDidMount() {
+    CountryModel.getAllCountries()
+      .then( (response) => {
+        console.log(`getAllCountries response: ${response.data}`);
+        // filter countries by region
+        let countriesAfrica = response.data.filter( (country) => {
+          return country.region_id === 1;
+        });
+
+        let countriesAsia = response.data.filter( (country) => {
+          return country.region_id === 2;
+        });
+
+        let countriesAus = response.data.filter( (country) => {
+          return country.region_id === 3;
+        });
+
+        let countriesEU = response.data.filter( (country) => {
+          return country.region_id === 4;
+        });
+
+        let countriesNAmer = response.data.filter( (country) => {
+          return country.region_id === 5;
+        });
+
+        let countriesSAmer = response.data.filter( (country) => {
+          return country.region_id === 6;
+        });
+
+        this.setState({
+          countriesAfrica: countriesAfrica,
+          countriesAsia: countriesAsia,
+          countriesAus: countriesAus,
+          countriesEU: countriesEU,
+          countriesNAmer: countriesNAmer,
+          countriesSAmer: countriesSAmer,
+        });
+      }).catch( (error) => {
+        console.log(`getAllCountries error: ${error}`);
+      });
+  };
+
+
   render() {
     return (
       <div className="footer columns">
@@ -10,50 +68,74 @@ class Footer extends Component {
           <div className="columns">
             <div className="column">
               <h6 className="subtitle is-6">Africa</h6>
-              <Link to="/country" className="is-size-7">Country 1</Link><br />
-              <Link to="/country" className="is-size-7">Country 2</Link><br />
-              <Link to="/country" className="is-size-7">Country 3</Link><br />
-              <Link to="/country" className="is-size-7">Country 4</Link>
+              {this.state.countriesAfrica.map(country => (
+                <div>
+                  <Link to="/country" 
+                  className="is-size-7"
+                  key={country._id}
+                  onClick={() => this.props.currentCountry(country)}>{country.countryName}</Link><br />
+                </div>
+              ))}
             </div>
 
             <div className="column">
               <h6 className="subtitle is-6">Asia</h6>
-              <Link to="/country" className="is-size-7">Country 1</Link><br />
-              <Link to="/country" className="is-size-7">Country 2</Link><br />
-              <Link to="/country" className="is-size-7">Country 3</Link><br />
-              <Link to="/country" className="is-size-7">Country 4</Link>
+              {this.state.countriesAsia.map(country => (
+                <div>
+                  <Link to="/country" 
+                  className="is-size-7"
+                  key={country._id}
+                  onClick={() => this.props.currentCountry(country)}>{country.countryName}</Link><br />
+                </div>
+              ))}
             </div>
 
             <div className="column">
               <h6 className="subtitle is-6">Australia</h6>
-              <Link to="/country" className="is-size-7">Country 1</Link><br />
-              <Link to="/country" className="is-size-7">Country 2</Link><br />
-              <Link to="/country" className="is-size-7">Country 3</Link><br />
-              <Link to="/country" className="is-size-7">Country 4</Link>
+              {this.state.countriesAus.map(country => (
+                <div>
+                  <Link to="/country" 
+                  className="is-size-7"
+                  key={country._id}
+                  onClick={() => this.props.currentCountry(country)}>{country.countryName}</Link><br />
+                </div>
+              ))}
             </div>
 
             <div className="column">
               <h6 className="subtitle is-6">Europe</h6>
-              <Link to="/country" className="is-size-7">Country 1</Link><br />
-              <Link to="/country" className="is-size-7">Country 2</Link><br />
-              <Link to="/country" className="is-size-7">Country 3</Link><br />
-              <Link to="/country" className="is-size-7">Country 4</Link>
+              {this.state.countriesEU.map(country => (
+                <div>
+                  <Link to="/country" 
+                  className="is-size-7"
+                  key={country._id}
+                  onClick={() => this.props.currentCountry(country)}>{country.countryName}</Link><br />
+                </div>
+              ))}
             </div>
 
             <div className="column">
               <h6 className="subtitle is-6">North America</h6>
-              <Link to="/country" className="is-size-7">Country 1</Link><br />
-              <Link to="/country" className="is-size-7">Country 2</Link><br />
-              <Link to="/country" className="is-size-7">Country 3</Link><br />
-              <Link to="/country" className="is-size-7">Country 4</Link>
+              {this.state.countriesNAmer.map(country => (
+                <div>
+                  <Link to="/country" 
+                  className="is-size-7"
+                  key={country._id}
+                  onClick={() => this.props.currentCountry(country)}>{country.countryName}</Link><br />
+                </div>
+              ))}
             </div>
 
             <div className="column">
               <h6 className="subtitle is-6">South America</h6>
-              <Link to="/country" className="is-size-7">Country 1</Link><br />
-              <Link to="/country" className="is-size-7">Country 2</Link><br />
-              <Link to="/country" className="is-size-7">Country 3</Link><br />
-              <Link to="/country" className="is-size-7">Country 4</Link>
+              {this.state.countriesSAmer.map(country => (
+                <div>
+                  <Link to="/country" 
+                  className="is-size-7"
+                  key={country._id}
+                  onClick={() => this.props.currentCountry(country)}>{country.countryName}</Link><br />
+                </div>
+              ))}
             </div>
           </div>
         </div>
@@ -67,4 +149,8 @@ class Footer extends Component {
   }
 }
 
-export default Footer;
+
+export default connect(
+  null,
+  { currentCountry }
+)(Footer);
