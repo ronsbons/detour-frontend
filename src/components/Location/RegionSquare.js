98e2d6179cfa673e64fed49ce9162d7069c125ae
ronsbons <ronnilouie@gmail.com>
2019-04-09 12:41:48 -0700

    adds hover and font styling to location squares

--- src/components/Location/RegionSquare.js
+++ src/components/Location/RegionSquare.js
@@ -13,15 +13,17 @@ class RegionSquare extends Component {
 
         {/* loop through the locations passed down from HomeContainer */}
         {this.props.regions.map(region => (
-          <div className="column is-one-third" key={region._id}>
+          <div className="column is-one-third region-square" key={region._id}>
             {/* .currentRegion is the action imported in */}
             {/* sends region object clicked on to change state */}
             <Link to="/region"
                   onClick={() => this.props.currentRegion(region)}>
-              <figure className="image is-256x256">
+              <figure className="image is-256x256 location-photo">
                 <img src={region.regionPhoto} alt={region.regionName} />
               </figure>
-              <h6 className="subtitle is-6">{region.regionName}</h6>
+              <div className="location-name-overlay">
+                <h2 className="subtitle is-2 location-name">{region.regionName}</h2>
+              </div>
             </Link>
 
           </div>
